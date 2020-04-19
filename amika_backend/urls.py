30fad3c8b6c4d033a639e34dfa49b0d028b3aa7a
azie-ginanjar <xstepnort@gmail.com>
2020-04-19 18:18:20 +0700

    fixed wording

--- amika_backend/urls.py
+++ amika_backend/urls.py
@@ -22,9 +22,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Amika Backend API",
       default_version='v1',
-      description="Test description",
+      description="Amika Backend",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
