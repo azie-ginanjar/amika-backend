c519b73b673a7f1669c28fe97700b45059dfba79
azie-ginanjar <xstepnort@gmail.com>
2020-04-20 08:02:12 +0700

    add endpoint to get details, update, and filter customer

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from customer.views import CustomerView
+from customer.views import CustomerCreate, CustomerList, CustomerDetails
 
 app_name = "customer"
 urlpatterns = [
-    path("", view=CustomerView.as_view(), name="customer-create-list"),
+    path("", view=CustomerCreate.as_view(), name="customer-create-list"),
+    path("<str:query>/list", view=CustomerList.as_view(), name='customer-list-filter'),
+    path("<int:pk>", view=CustomerDetails.as_view(), name='customer-update-details'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
 from django.shortcuts import render
 
-
 # Create your views here.
 from rest_framework import generics
 
@@ -9,19 +8,22 @@ from customer.models import Customer
 from customer.serializers import CustomerSerializer
 
 
-class CustomerView(generics.ListCreateAPIView):
-    """
-        query -- customer name / phone number
-    """
+class CustomerCreate(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     # permission_classes = (AdminRequired, )
     serializer_class = CustomerSerializer
 
+
+class CustomerList(generics.ListAPIView):
+    serializer_class = CustomerSerializer
+
     def get_queryset(self):
-        query = self.request.query_params.get('query', None)
+        query = self.kwargs['query']
 
-        queryset = Customer.objects.all()
-        if query:
-            queryset = queryset.filter(Q(name=query) | Q(phone_number=query))
+        return Customer.objects.filter(Q(name=query) | Q(phone_number=query))
 
-        return queryset
+
+class CustomerDetails(generics.RetrieveUpdateAPIView):
+    queryset = Customer.objects.all()
+    # permission_classes = (AdminRequired, )
+    serializer_class = CustomerSerializer
