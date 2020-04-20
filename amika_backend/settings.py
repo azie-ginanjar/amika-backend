b001d420de47e0a41eb96ea335457d32313d7e3a
azie-ginanjar <xstepnort@gmail.com>
2020-04-20 09:30:34 +0700

    retrieve token lifetime from env variable

--- amika_backend/settings.py
+++ amika_backend/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=env.int("ACCESS_TOKEN_LIFETIME", 5)),
     'REFRESH_TOKEN_LIFETIME': timedelta(weeks=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
